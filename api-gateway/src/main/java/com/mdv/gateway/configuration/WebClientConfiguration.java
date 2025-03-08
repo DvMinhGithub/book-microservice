01e185876f1261fe9826d31d7bcfb0e568f01585
Minh Dinh <minhdv.dev@gmail.com>
2025-03-08 14:19:49 +0700

    Add CORS configuration to API Gateway

--- api-gateway/src/main/java/com/mdv/gateway/configuration/WebClientConfiguration.java
+++ api-gateway/src/main/java/com/mdv/gateway/configuration/WebClientConfiguration.java
@@ -2,6 +2,9 @@ package com.mdv.gateway.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -21,5 +24,18 @@ public class WebClientConfiguration {
                 .build();
                 
         return factory.createClient(IdentityClient.class);
+    }
+
+    @Bean
+    CorsWebFilter corsFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.addAllowedOrigin("*");
+        corsConfig.addAllowedHeader("*");
+        corsConfig.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(urlBasedCorsConfigurationSource);
     }
 }
